8ec17173eef178b0af1a6c81c801999e4f0ea0c9
bessapple14 <89636826+bessapple14@users.noreply.github.com>
2021-10-25 11:11:42 +0000

    데이터 관련 설정을 별도 페이지로 분리

--- APP(Android)/jilju/lib/setting.dart
+++ APP(Android)/jilju/lib/setting.dart
@@ -1,13 +1,10 @@
-import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter/services.dart';
 import 'package:jilju/theme.dart';
 import 'package:numberpicker/numberpicker.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 
-import 'database.dart';
-import 'main.dart';
 import 'message.dart';
+import 'setting_data.dart';
 import 'util.dart';
 
 class SettingPage extends StatefulWidget {
@@ -20,7 +17,6 @@ class SettingPage extends StatefulWidget {
 class _SettingPageState extends State<SettingPage> {
   late int _userWeight, _numberPickerValue;
   late JiljuTheme _jiljuTheme, _themeSelectionValue;
-  final TextEditingController _backupCodeController = TextEditingController();
 
   Future<int?> _showWeightInputDialog() async {
     _numberPickerValue = _userWeight;
@@ -118,92 +114,6 @@ class _SettingPageState extends State<SettingPage> {
         );
       },
     );
-  }
-
-  Future<void> _showBackupCompleteDialog(String backupCode) async {
-    return showDialog(
-      context: context,
-      builder: (context) {
-        return AlertDialog(
-          title: const Text('백업 완료'),
-          content: Wrap(
-            children: <Widget>[
-              Column(
-                children: <Widget>[
-                  Row(
-                    children: <Widget>[
-                      const Expanded(
-                        child: Text('백업 코드', style: TextStyle(fontSize: 16)),
-                      ),
-                      IconButton(
-                        icon: const Icon(Icons.content_copy, size: 14),
-                        onPressed: () {
-                          Clipboard.setData(ClipboardData(text: backupCode));
-                        },
-                        splashRadius: 14,
-                      ),
-                    ],
-                  ),
-                  Center(
-                    child: TextField(
-                      controller: TextEditingController()..text = backupCode,
-                      style: const TextStyle(fontSize: 14),
-                      textAlign: TextAlign.center,
-                    ),
-                  ),
-                  const SizedBox(height: 20),
-                  Text(
-                    MessageManager.messageString[14],
-                    style: const TextStyle(fontSize: 12),
-                  ),
-                ],
-              ),
-            ],
-          ),
-          actions: <Widget>[
-            TextButton(
-              child: const Text('OK'),
-              onPressed: () => Navigator.pop(context),
-            ),
-          ],
-        );
-      },
-    );
-  }
-
-  Future<String?> _showBackupCodeInputDialog() async {
-    return showDialog(
-      context: context,
-      builder: (context) {
-        _backupCodeController.clear();
-        return AlertDialog(
-          title: const Text('백업 코드 입력'),
-          content: TextField(
-            controller: _backupCodeController,
-            inputFormatters: <TextInputFormatter>[
-              FilteringTextInputFormatter.allow(RegExp(r'[A-Za-z0-9]')),
-            ],
-          ),
-          actions: <Widget>[
-            TextButton(
-              child: const Text('CANCEL'),
-              onPressed: () => Navigator.pop(context),
-            ),
-            TextButton(
-              child: const Text('OK'),
-              onPressed: () =>
-                  Navigator.pop(context, _backupCodeController.text),
-            ),
-          ],
-        );
-      },
-    );
-  }
-
-  void _setProgressVisible(bool visible) {
-    context
-        .findAncestorStateOfType<JiljuMainPageState>()!
-        .setProgressVisible(visible);
   }
 
   @override
@@ -312,110 +222,20 @@ class _SettingPageState extends State<SettingPage> {
                       children: const <Widget>[
                         Expanded(
                           child: Text(
-                            '데이터 백업하기',
+                            '데이터 관리',
                             style: TextStyle(fontSize: 20),
                           ),
                         ),
                       ],
                     ),
                   ),
-                  onTap: () async {
-                    _setProgressVisible(true);
-                    DocumentReference df = await FirebaseFirestore.instance
-                        .collection('jilju')
-                        .add({
-                      'data': await DatabaseManager.toJson(),
-                      'timestamp': DateTime.now().millisecondsSinceEpoch,
-                    });
-                    _setProgressVisible(false);
-                    await _showBackupCompleteDialog(df.id);
-                  },
-                ),
-              ),
-              Card(
-                margin:
-                    const EdgeInsets.symmetric(vertical: 10, horizontal: 20),
-                shape: RoundedRectangleBorder(
-                  borderRadius: BorderRadius.circular(16),
-                ),
-                elevation: 0,
-                child: InkWell(
-                  child: Padding(
-                    padding: const EdgeInsets.symmetric(
-                        vertical: 10, horizontal: 20),
-                    child: Row(
-                      children: const <Widget>[
-                        Expanded(
-                          child: Text(
-                            '데이터 복원하기',
-                            style: TextStyle(fontSize: 20),
-                          ),
-                        ),
-                      ],
-                    ),
-                  ),
-                  onTap: () async {
-                    String? backupCode = await _showBackupCodeInputDialog();
-                    if (backupCode == null) {
-                      return;
-                    }
-                    _setProgressVisible(true);
-                    DocumentSnapshot ds = await FirebaseFirestore.instance
-                        .collection('jilju')
-                        .doc(backupCode)
-                        .get();
-                    _setProgressVisible(false);
-                    if (ds.data() == null) {
-                      await MessageManager.showMessageDialog(context, 15);
-                      return;
-                    }
-                    Map<String, dynamic> data =
-                        ds.data() as Map<String, dynamic>;
-                    bool? restore =
-                        await MessageManager.showYesNoDialog(context, 16);
-                    if (restore == null || !restore) {
-                      return;
-                    }
-                    _setProgressVisible(true);
-                    await DatabaseManager.clearAllData();
-                    await DatabaseManager.fromJson(data['data']);
-                    await MessageManager.showMessageDialog(context, 17);
-                    _setProgressVisible(false);
-                  },
-                ),
-              ),
-              Card(
-                margin:
-                    const EdgeInsets.symmetric(vertical: 10, horizontal: 20),
-                shape: RoundedRectangleBorder(
-                  borderRadius: BorderRadius.circular(16),
-                ),
-                elevation: 0,
-                child: InkWell(
-                  child: Padding(
-                    padding: const EdgeInsets.symmetric(
-                        vertical: 10, horizontal: 20),
-                    child: Row(
-                      children: const <Widget>[
-                        Expanded(
-                          child: Text(
-                            '모든 데이터 지우기',
-                            style: TextStyle(fontSize: 20),
-                          ),
-                        ),
-                      ],
-                    ),
-                  ),
-                  onTap: () async {
-                    bool? clear =
-                        await MessageManager.showYesNoDialog(context, 12);
-                    if (clear == null || !clear) {
-                      return;
-                    }
-                    _setProgressVisible(true);
-                    await DatabaseManager.clearAllData();
-                    _setProgressVisible(false);
-                    await MessageManager.showMessageDialog(context, 13);
+                  onTap: () {
+                    Navigator.push(
+                      context,
+                      MaterialPageRoute(
+                        builder: (context) => const SettingDataPage(),
+                      ),
+                    );
                   },
                 ),
               ),
